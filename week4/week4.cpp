86978ca9e7e1ac72fb527f6e606fb27577dbc5c6
NathanCTai <nct36@cornell.edu>
2026-03-23 21:00:19 -0400

    implemented two-fare recalculation strategy (it does really bad for some reason)

---
+++ week4/week4.cpp
@@ -0,0 +1,130 @@
+#include <vector>
+#include <random>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+const int T = 100, P = 41, Runs = 1000, Sims = 6, gap = 10; int C = 0;
+vector<vector<int>> TFPreCompP1DX(T), DPsugIDX(T);
+vector<vector<float>> Lambda(T);
+vector<float> Alpha(T, 0.0f), Gamma(T), Prices(P);
+random_device rd; unsigned int seed = rd(); mt19937 Gen(seed);
+uniform_real_distribution<float> AlphaDist(0.5f, 1.0f), GammaDist(0.0f, 0.1f), Coin(0.0f, 1.0f);
+
+void resetData() {
+    fill(Alpha.begin(), Alpha.end(), 0.0f);
+    fill(Gamma.begin(), Gamma.end(), 0.0f);
+    Lambda.assign(T, vector<float>(P, 0.0f));
+    TFPreCompP1DX.assign(T, vector<int>(P, 0));
+}
+
+void readData() {
+    for (int t = 0; t < T; t++) {
+        Alpha[t] = AlphaDist(Gen);
+        Gamma[t] = 1.2 - exp(-float(t) / 451) - GammaDist(Gen);
+        for (int p = 0; p < P; p++) {
+            if (t == 0) { Prices[p] = float(p) / 2.0; }
+            Lambda[t][p] = exp(-Gamma[t] * Prices[p]);
+        }
+    }
+    C = int(accumulate(Alpha.begin(), Alpha.end(), 0.0f) * 0.3);
+}
+
+float calcDP() {
+    vector<vector<float>> DP(T + 1, vector<float>(C, 0.0f));
+    for (int t = T - 1; t > -1; t--) { 
+        DPsugIDX[t].assign(C, 0); // DP suggests price INDICES to charge at 
+        for (int x = 1; x < C; x++) {
+            float maximum = 0.0;
+            for (int p = 0; p < P; p++) { 
+                float lhs = Alpha[t] * Lambda[t][p] * (Prices[p] + DP[t + 1][x - 1]);
+                float rhs = (1 - Alpha[t] * Lambda[t][p]) * DP[t + 1][x];
+                if (lhs + rhs > maximum) {
+                    maximum = rhs + lhs; DPsugIDX[t][x] = p;
+                } 
+            } DP[t][x] = maximum;
+        }
+    } return DP[0][C - 1];
+}
+
+float simDP() {
+    float rev = 0.0;
+    for (int run = 0; run < Runs; run++) {
+        int stock = C; 
+        for (int t = 0; t < T; t++) {
+            int priceIDX = DPsugIDX[t][stock];
+            if (Coin(Gen) < Alpha[t] && Coin(Gen) < Lambda[t][priceIDX] && stock > 0) {
+                stock -= 1; rev += Prices[priceIDX];
+            }
+        }
+    }
+    return rev / Runs;
+}
+
+float staticOneFareSim() {
+    vector<float> rev(P, 0.0f);
+    for (int run = 0; run < Runs; run++) {
+        vector<int> stock(P, C - 1);
+        for (int t = 0; t < T; t++) {
+            for (int p = 0; p < P; p++) {
+                if (Coin(Gen) < Alpha[t] && Coin(Gen) < Lambda[t][p] && stock[p] > 0) {
+                    stock[p] -= 1; rev[p] += Prices[p];
+                }
+            }
+        }
+    }
+    return *max_element(rev.begin(), rev.end()) / Runs;
+}
+
+void fillTFPreComp() { // Stores the INDICES of the optimal P1 at every time/capacity combination
+    for (int t = 0; t < T; t++) {
+        TFPreCompP1DX[t].assign(C, 0);
+        for (int c = 0; c < C; c++) {
+            int split = T * 0.2 + t * 0.8;
+            // int split = T * 0.5 + t * 0.5;
+            float best_rev = 0; int best_p1_idx = 0;
+            for (int p1 = 0; p1 < P; p1++) {
+                for (int p2 = 0; p2 < P; p2++) {
+                    float theta1 = 0.0f, theta2 = 0.0f;
+                    for (int u = 0; u < split; u++)  theta1 += Alpha[t] * Lambda[t][p1];
+                    for (int u = split; u < T; u++)  theta2 += Alpha[t] * Lambda[t][p2];
+                    float exp1 = min(float(c), theta1);
+                    float calc_rev = Prices[p1] * exp1 + Prices[p2] * min(float(c) - exp1, theta2);
+                    if (calc_rev > best_rev) { best_p1_idx = p1; best_rev = calc_rev; }
+                    // printf("calc_rev: %f, best_rev: %f \n", calc_rev, best_rev);
+                }
+            }
+            TFPreCompP1DX[t][c] = best_p1_idx; 
+            // printf("TFPrecomp[%d][%d] = %d \n", t, c, best_p1_idx);
+        }
+    }
+}
+
+float simTFRecalc(int gap) {
+    float rev = 0.0f;
+    for (int run = 0; run < Runs; run++) {
+        int stock = C; 
+        for (int s = 0; s < T; s = s + gap) {
+            for (int t = s; t < s + gap; t++) {
+                int p = TFPreCompP1DX[s][stock];
+                if (Coin(Gen) < Alpha[t] && Coin(Gen) < Lambda[t][p] && stock > 0) {
+                    stock -= 1; rev += Prices[p];
+                    // printf("rev: %f, price: %f \n", rev, Prices[p]);
+                }
+            }
+        }
+    }
+    return rev / Runs;
+}
+
+int main() {
+    ofstream data("data.csv");
+    data << "Two_Fare_Recalc_Sim,DP_Calc,DP_Sim,One_Fare_Static_Sim\n";
+    for (int sim = 0; sim < Sims; sim++) {
+        resetData();
+        readData();
+        fillTFPreComp();
+        data << simTFRecalc(10) << "," << calcDP() << "," << simDP() << "," << staticOneFareSim() << "\n";
+    }
+    return 0;
+}
